896319c4406b02f1652ac5906f59ce8e31c4cd7c
chzellweger <chzellweger@gmail.com>
2016-06-05 22:56:06 +0200

    whitespaces and commas

--- app/show.js
+++ app/show.js
@@ -8,18 +8,18 @@ function Show(props) {
   if (props.insideStage) {
     return (
       <div className='show'>
-      <div className='show-decorator'>*-*-*-*</div>
-      <div className='show-time'>{props.showData.time}</div>
-      <div className='show-name band'>{props.showData.name}</div>
+        <div className='show-decorator'>*-*-*-*</div>
+        <div className='show-time'>{props.showData.time}</div>
+        <div className='show-name band'>{props.showData.name}</div>
       </div>
     );
   } else {
     return (
       <div className='show'>
-      <div className='show-decorator'>*-*-*-*</div>
-      <div className='show-time'>{props.showData.time}</div>
-      <div className='show-name band'>{props.showData.name}</div>
-      <div className='show-stage'>{props.showData.stage}</div>
+        <div className='show-decorator'>*-*-*-*</div>
+        <div className='show-time'>{props.showData.time}</div>
+        <div className='show-name band'>{props.showData.name}</div>
+        <div className='show-stage'>{props.showData.stage}</div>
       </div>
     );
   }
